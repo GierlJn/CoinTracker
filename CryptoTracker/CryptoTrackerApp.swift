f65e0caeace1c2b9de5e94bfdfb3c11f2f71b09f
Julian <julian.gierl@gmx.de>
2022-04-14 13:54:59 +0200

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Julian Gierl on 14.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
