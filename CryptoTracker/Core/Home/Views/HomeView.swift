95d557b31dba4bae709ae58c44763e99cd1d4259
Julian <julian.gierl@gmx.de>
2022-04-14 15:14:34 +0200

    added CoinModel

--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -11,43 +11,52 @@ struct HomeView: View {
   
   @State private var showPortfolio: Bool = false
   
-    var body: some View {
-      ZStack{
-        Color.colorTheme.background
-          .ignoresSafeArea()
-        
-        VStack{
-          HStack{
-            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
-              .background{
-                CircleButtonAnimationView(animate: $showPortfolio)
-              }
-            Spacer()
-            Text(showPortfolio ? "Portfolio" : "Live Prices")
-              .font(.headline)
-              .fontWeight(.heavy)
-              .foregroundColor(Color.colorTheme.accent)
-              .animation(.none)
-            Spacer()
-            CircleButtonView(iconName: "chevron.right")
-              .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-              .onTapGesture {
-                withAnimation(.spring()){
-                  showPortfolio.toggle()
-                }
-              }
-          }
-          Spacer()
-        }
+  var body: some View {
+    ZStack{
+      Color.colorTheme.background
+        .ignoresSafeArea()
+      
+      VStack{
+        headerView
+        Spacer()
       }
     }
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-      NavigationView{
-        HomeView()
-          .navigationBarHidden(true)
-      }
+  static var previews: some View {
+    NavigationView{
+      HomeView()
+        .navigationBarHidden(true)
     }
+  }
+}
+
+extension HomeView{
+  
+  var headerView: some View{
+    HStack{
+      CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+        .background{
+          CircleButtonAnimationView(animate: $showPortfolio)
+        }
+      Spacer()
+      Text(showPortfolio ? "Portfolio" : "Live Prices")
+        .font(.headline)
+        .fontWeight(.heavy)
+        .foregroundColor(Color.colorTheme.accent)
+        .animation(.none)
+      Spacer()
+      CircleButtonView(iconName: "chevron.right")
+        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+        .onTapGesture {
+          withAnimation(.spring()){
+            showPortfolio.toggle()
+          }
+        }
+        .padding(.horizontal)
+    }
+    
+  }
 }
