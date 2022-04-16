7bd956df1d18085de68761c35564392e21b17bff
Julian <julian.gierl@gmx.de>
2022-04-16 11:29:11 +0200

    refactored network layer

--- CryptoTracker/Services/CoinDataService.swift
+++ CryptoTracker/Services/CoinDataService.swift
@@ -20,22 +20,9 @@ class CoinDataService{
   func getCoins(){
     guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
     
-    cancellable = URLSession.shared.dataTaskPublisher(for: url)
-      .subscribe(on: DispatchQueue.global(qos: .default))
-      .tryMap({ (output) -> Data in
-        guard let urlResponse = output.1 as? HTTPURLResponse, urlResponse.statusCode == 200 else { throw URLError(.badServerResponse) }
-        return output.data
-      })
-      .receive(on: DispatchQueue.main)
+    cancellable = NetworkManager.download(url: url)
       .decode(type: [CoinModel].self, decoder: JSONDecoder())
-      .sink(receiveCompletion: { completion in
-        switch completion{
-        case .finished:
-          break
-        case .failure(let error):
-          print(error.localizedDescription)
-        }
-      }, receiveValue: { [unowned self] coins in
+      .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [unowned self] coins in
         self.allCoins = coins
         self.cancellable?.cancel()
       })
---
+++ CryptoTracker/Services/NetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkManager.swift
+//  CryptoTracker
+//
+//  Created by Julian Gierl on 15.04.22.
+//
+
+import Foundation
+import Combine
+
+enum NetworkError: LocalizedError{
+  case badURlResponse(url: URL)
+  case unknown
+  
+  var errorDescription: String?{
+    switch self{
+    case .badURlResponse(let url):
+      return "🔥 Bad URL Response: \(url)"
+    case .unknown:
+      return "⚠️ Unknown Error"
+    }
+  }
+}
+
+class NetworkManager{
+  
+  static func download(url: URL)->AnyPublisher<Data, Error>{
+    URLSession.shared.dataTaskPublisher(for: url)
+      .subscribe(on: DispatchQueue.global(qos: .default))
+      .tryMap{ try handleUrlResponse(output: $0, url: url) }
+      .receive(on: DispatchQueue.main)
+      .eraseToAnyPublisher()
+  }
+  
+  static func handleUrlResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data{
+    guard let urlResponse = output.1 as? HTTPURLResponse, urlResponse.statusCode == 200 else { throw NetworkError.badURlResponse(url: url) }
+    return output.data
+  }
+  
+  static func handleCompletion(completion:  Subscribers.Completion<Publishers.Decode<AnyPublisher<Data, Error>, [CoinModel], JSONDecoder>.Failure>){
+    switch completion{
+    case .finished:
+      break
+    case .failure(let error):
+      print(error.localizedDescription)
+    }
+  }
+  
+}
