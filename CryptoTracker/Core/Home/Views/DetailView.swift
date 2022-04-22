77f7018d8640083da37fc40d626b9b0cf485a50c
Julian <julian.gierl@gmx.de>
2022-04-22 22:24:25 +0200

    coinmodel

---
+++ CryptoTracker/Core/Home/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  CryptoTracker
+//
+//  Created by Julian Gierl on 22.04.22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+  let coin: CoinModel
+    var body: some View {
+      Text(coin.name)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+      DetailView(coin: CoinModel.mockData)
+    }
+}
